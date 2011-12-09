1da8ba877d33bc603d6973a55d7ce43b20db3f93
Schell Scivally <efsubenovex@gmail.com>
2011-12-09 09:54:11 -0800

    first commit - just some tests for packing states and validating states and transitions.

---
+++ README.md
@@ -0,0 +1,58 @@
+aagol
+=====
+aagol is "another automaton game of life".
+
+wtf does it do?
+---------------
+aagol is a system of states and rules that transition into new states based on those rules.
+
+is it any good?
+---------------
+meh.
+
+rules
+-----
+Users program lifeforms and watch them transition and transform. It is similar to Conway's game of life. It consists of a number of parts
+    1. The server (the world):
+        The world consists of a 2d grid of spaces in a given state. 
+        States:
+            0 Empty 
+            1 Block
+            2 Program
+            3 Resource 
+        Rules:
+            Programs can move into empty spaces
+            Programs can switch places with blocks
+            Programs that surround a resource on two sides turn that resource into a like program (replicate)
+            Programs that have no neighboring like programs turn into empty space (die)
+            Unlike programs are resources (for your programs)
+            Newer programs have iteration priority
+            
+        The server would take care of ticking the world forward based on certain transitions the cells make. Life cells' transitions are programmable by the user.
+        
+    2. The client:
+        Receives game state and displays it. Could possibly just receive the initial game state and transitions to cut down on data transmission.
+        
+    3. The language:
+        Defines transitions of programs. The user defines a pre and post transition state using a char array in which each char represents a cell. 
+        The user defines a 3x3 block of the world as a string of states 0-3 or X (the program being iterated over):
+            000 000    
+            2X0 20X 
+            000 000
+        This transition shows a program that is right of a like program in otherwise empty space moving to the right.
+            000 000     
+            0X0 X0X
+            020 020
+        This transition shows a program above a like program moving EITHER left OR right.
+            000 XXX
+            0X0 X0X
+            020 X2X
+        Randomly move anywhere (except to the space already occupied by a like program).
+            000 000
+            0X0 000
+            202 2X2
+        Move down in between two like programs.
+            003 0X3
+            0X0 000
+            020 020
+        Move in on a resource.
---
+++ agol.js
@@ -0,0 +1,126 @@
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *	agol.js
+ *	Another Automaton Game Of Life
+ *	Copyright (c) 2011 Schell Scivally Enterprise. All rights reserved.
+ *
+ *	@author	Schell Scivally
+ *	@since	Thu Dec  8 17:49:46 PST 2011
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+var agol = mod({
+	name : 'agol',
+	dependencies : [],
+	init : function (modules) {
+		/**
+		 *	Initializes the agol object
+		 */
+		var states = {
+			/**
+			 *	Our cell states.
+			 */
+			0 : 'empty',
+			1 : 'block',
+			2 : 'program',
+			3 : 'resource'
+		};
+		var packState = function (expandedState) {
+			/**
+			 * Packs a state from a (possibly) expanded representation
+			 * 000 
+			 * 0X0
+			 * 000
+			 * into 0000X0000
+			 */
+			expandedState = expandedState.replace(/\n|\s/g, '');
+			return expandedState;
+		};
+		
+		var isValidState = function (state) {
+			/**
+			 *	Checks a state for validity.
+			 */
+			state = packState(state);
+			var regex = /^[0-3|X]{9}$/;
+			var match = state.match(regex);
+			if (match && match.length == 1 && match[0] == state) {
+				return true;
+			}
+			console.warn('invalid state:',state);
+			return false;
+		};
+		
+		var isValidPretransition = function (state) {
+			/**
+			 *	Checks a pretransition for validity.
+			 */
+			var packed = packState(state);
+			if (!isValidState(packed)) {
+				console.warn('invalid pretransition:',state,'(is invalid state)');
+				return false;
+			}
+			if (packed[4] !== 'X') {
+				console.warn('invalid pretransition:',state,'(program is not in center of state)');
+				return false;
+			}
+			return true;
+		};
+		
+		var isValidTransition = function (pre, post) {
+			/**
+			 *	Checks to see if given *pre*, *post* is valid.
+			 */
+			packedPre = packState(pre);
+			packedPost = packState(post);
+			if (packedPre == packedPost) {
+				console.warn('invalid transition:',pre,post,'no transition occurs');
+				return false;
+			}
+			if (!isValidState(packedPre)) {
+				console.warn('invalid transition:',pre,post,'pretransition is invalid state');
+				return false;
+			}
+			if (!isValidState(packedPost)) {
+				console.warn('invalid transition:',pre,post,'posttransition is invalid state');
+				return false;
+			}
+			var getNdxsOf = function (char, string) {
+				var ndxs = [];
+				var n = string.length;
+				for (var i = n - 1; i >= 0; i--) {
+					if (string[i] == char) {
+						ndxs.push(i);
+					}
+				}
+				return ndxs;
+			};
+			
+			var ndxsAreEq = function (ndx1, ndx2) {
+				ndx1.map(function (el,i,a) {
+					if (el !== i) {
+						return false;
+					}
+				});
+				return true;
+			};
+			// check to see that resources are untouched
+			var preResNdxs = getNdxsOf('3', packedPre);
+			var postResNdxs = getNdxsOf('3', packedPost);
+			if (!ndxsAreEq(preResNdxs, postResNdxs)) {
+				console.warn('invalid transition:',pre,post,'cannot move resources');
+				return false;
+			}
+			// check to see that program has moved only into empty space
+			// or switched places with a block
+			
+			return true;
+		};
+		
+		
+		
+		return {
+			packState : packState,
+			isValidState : isValidState,
+			isValidPretransition : isValidPretransition,
+			isValidTransition : isValidTransition
+		};
+	}
+});
