7f652a666bb5341c0e24f77a3dc315a88625f285
Schell Scivally <efsubenovex@gmail.com>
2011-12-09 16:43:11 -0800

    initial editor capabilities

--- aagol.js
+++ aagol.js
@@ -23,6 +23,7 @@ var aagol = mod({
 			'3' : 'resource',
 			'X' : 'program'
 		};
+		
 		var packState = function (expandedState) {
 			/**
 			 * Packs a state from a (possibly) expanded representation
@@ -35,6 +36,18 @@ var aagol = mod({
 			return expandedState;
 		};
 		
+		var logs = [];
+		var log = function () {
+			console.warn(arguments);
+			logs.push(Array.prototype.slice.call(arguments));
+		};
+		var clearLog = function () {
+			logs = [];
+		};
+		var getLog = function () {
+			return logs;
+		};
+		
 		var isValidState = function (state) {
 			/**
 			 *	Checks a state for validity.
@@ -45,7 +58,7 @@ var aagol = mod({
 			if (match && match.length == 1 && match[0] == state) {
 				return true;
 			}
-			console.warn('invalid state:',state);
+			log('invalid state:',state,'state can only characters 0-3 or X and must be 9 characters long');
 			return false;
 		};
 		
@@ -55,11 +68,11 @@ var aagol = mod({
 			 */
 			var packed = packState(state);
 			if (!isValidState(packed)) {
-				console.warn('invalid pretransition:',state,'(is invalid state)');
+				log('invalid pretransition:',state,'(is invalid state)');
 				return false;
 			}
 			if (packed[4] !== 'X') {
-				console.warn('invalid pretransition:',state,'(program is not in center of state)');
+				log('invalid pretransition:',state,'(program is not in center of state)');
 				return false;
 			}
 			return true;
@@ -72,19 +85,19 @@ var aagol = mod({
 			packedPre = packState(pre);
 			packedPost = packState(post);
 			if (packedPre == packedPost) {
-				console.warn('invalid transition:',pre,post,'no transition occurs');
+				log('invalid transition:',pre,post,'no transition occurs');
 				return false;
 			}
 			if (!isValidState(packedPre)) {
-				console.warn('invalid transition:',pre,post,'pretransition is invalid state');
+				log('invalid transition:',pre,post,'pretransition is invalid state');
 				return false;
 			}
 			if (!isValidState(packedPost)) {
-				console.warn('invalid transition:',pre,post,'posttransition is invalid state');
+				log('invalid transition:',pre,post,'posttransition is invalid state');
 				return false;
 			}
 			if (!isValidPretransition(packedPre)) {
-				console.warn('invalid transition:',pre,'pretransition is not valid');
+				log('invalid transition:',pre,'pretransition is not valid');
 			}
 			var getNdxsOf = function (char, string) {
 				/**
@@ -134,7 +147,7 @@ var aagol = mod({
 				var preNdxs = getNdxsOf(char, packedPre);
 				var postNdxs = getNdxsOf(char, packedPost);
 				if (!ndxsAreEq(preNdxs, postNdxs)) {
-					console.warn('invalid transition:',pre,post,'cannot add, delete or move '+states[char]);
+					log('invalid transition:',pre,post,'cannot add, delete or move '+states[char]);
 					return false;
 				}
 				return true;
@@ -153,7 +166,7 @@ var aagol = mod({
 			var pstBlkNdxs = getNdxsOf('1', packedPost);
 			if (preBlkNdxs.length != pstBlkNdxs.length) {
 				// a block has been added or deleted
-				console.warn('invalid transition:',pre,post,'cannot add, delete or replace a block');
+				log('invalid transition:',pre,post,'cannot add, delete or replace a block');
 				return false;
 			}
 			if (!ndxsAreEq(preBlkNdxs, pstBlkNdxs)) {
@@ -161,14 +174,14 @@ var aagol = mod({
 				var preProgNdxs = getNdxsOf('X', packedPre);
 				var postProgNdxs = getNdxsOf('X', packedPost);
 				if (preProgNdxs.length != postProgNdxs) {
-					console.warn('invalid transition:',pre,post,'cannot add, delete or replace program and switch places with a block');
+					log('invalid transition:',pre,post,'cannot add, delete or replace program and switch places with a block');
 					return false;
 				}
 				
 				var diffNdxs = diff(packedPre, packedPost);
 				if (diffNdxs.length > 2) {
 					// more than one block has moved
-					console.warn('invalid transition:',pre,post,'a program cannot move more than one block');
+					log('invalid transition:',pre,post,'a program cannot move more than one block');
 					return false;
 				}
 			}
@@ -176,17 +189,139 @@ var aagol = mod({
 			return true;
 		};
 		
-		var createStateEditor = function () {
+		var createProgramEditor = function () {
+			var editor = document.createElement('fieldset');
+			editor.id = 'stateEditor';
 			
+			var title = document.createElement('legend');
+			title.id = 'stateEditor_title';
+			title.innerHTML = 'aagol program editor';
+			editor.appendChild(title);
+			
+			var areaCSS = 'width:3.5em; height:5em; resize:none; padding:0.5em;';
+			
+			var preArea = document.createElement('textArea');
+			preArea.id = 'pre_editor';
+			preArea.style.cssText = areaCSS;
+			editor.appendChild(preArea);
+			
+			var postArea = document.createElement('textArea');
+			postArea.id = 'post_editor';
+			postArea.style.cssText = areaCSS;
+			editor.appendChild(postArea);
+			
+			var br = document.createElement('br');
+			editor.appendChild(br);
+			
+			var addButton = document.createElement('input');
+			addButton.type = 'submit';
+			addButton.value = 'add';
+			addButton.disabled = true;
+			editor.appendChild(addButton);
+			
+			var output = document.createElement('fieldset');
+			output.id = 'editor_output';
+			
+			var outputTitle = document.createElement('legend');
+			outputTitle.innerHTML = 'console';
+			output.appendChild(outputTitle);
+			editor.appendChild(output);
+			
+			var outputValue = document.createElement('div');
+			outputValue.id = 'editor_output_value';
+			output.appendChild(outputValue);
+			
+			var changeOutput = function (isValid) {
+				var msg = '';
+			
+				if (!isValid) {
+					outputValue.style.cssText = 'color:red;';
+					var n = logs.length;
+					for (var i = n - 1; i >= 0; i--) {
+						var args = logs[i];
+						for (var j = 0; j < args.length; j++) {
+							msg += args[j].toString() + ' - ';
+						}
+						msg += '<br/>';
+					}
+				} else {
+					outputValue.style.cssText = 'color:green;';
+					msg = 'okay!';
+				}
+			
+				document.getElementById('editor_output_value').innerHTML = msg;
+			}
+			
+			var prechange = function () {
+				preArea.value = packState(preArea.value.substring(0, 9));
+				logs = [];
+				var isValid = isValidPretransition(preArea.value);
+				var msg = '';
+			
+				changeOutput(isValid);
+				return isValid;
+			};
+			
+			var postchange = function () {
+				postArea.value = packState(postArea.value.substring(0, 9));
+				logs = [];
+				var isValid = isValidState(postArea.value);
+				changeOutput(isValid);
+				return isValid;
+			};
+			
+			var testChange = function () {
+				addButton.disabled = true;
+				if (preArea.value != '' && prechange()) {
+					if (postArea.value != '' && postchange()) {
+						logs = [];
+						var isValid = isValidTransition(preArea.value, postArea.value);
+						changeOutput(isValid);
+						if (isValid) {
+							addButton.disabled = false;
+						}
+					}
+				}
+			};
+			
+			preArea.onchange = testChange;
+			postArea.onchange = testChange;
+			
+			var pgmDefSet = document.createElement('fieldset');
+			editor.appendChild(pgmDefSet);
+			var pgmTitle = document.createElement('legend');
+			pgmTitle.innerHTML = 'program defintion';
+			pgmDefSet.appendChild(pgmTitle);
+			editor.appendChild(pgmDefSet);
+			var pgmStates = document.createElement('div');
+			pgmDefSet.appendChild(pgmStates);
+			
+			var states = 0;
+			var addTransition = function (pre, post) {
+				var div = document.createElement('div');
+				div.id = 'state_'+(states++).toString();
+				div.style.cssText = 'background-color:#AFAFAF; padding:0.5em; float:left;';
+				div.innerHTML = pre + ' ' + post;
+				pgmStates.appendChild(div);
+			};
+			
+			addButton.onclick = function () {
+				addTransition(preArea.value, postArea.value);
+			};
+			
+			return editor;
 		};
 		
 		
 		return {
+			states : states,
+			clearLog : clearLog,
+			getLog : getLog,
 			packState : packState,
 			isValidState : isValidState,
 			isValidPretransition : isValidPretransition,
 			isValidTransition : isValidTransition,
-			createStateEditor : createStateEditor
+			createProgramEditor : createProgramEditor
 		};
 	}
 });
